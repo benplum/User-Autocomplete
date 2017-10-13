a3b9bc69ea162dd8c79c8afe25d9b780733365f6
Ben Plum <mr@benplum.com>
2017-10-12 20:07:23 -0400

    Insert a user name if found

--- src/components/autocomplete.vue
+++ src/components/autocomplete.vue
@@ -2,10 +2,10 @@
   <div>
 
     <!-- contenteditable gives us more control over range selection -->
-    <div contenteditable="true" :class="className" ref="input" @input="onInput()"></div>
+    <div contenteditable="true" :class="className" @input="onInput()" @keydown.up="onUp($event)" @keydown.down="onDown($event)" @keydown.enter="onEnter($event)"></div>
 
     <div class="results_list">
-      <div class="results_item" v-for="(item, index) in results">
+      <div class="results_item" v-for="(item, index) in results" :class="{ 'active': (index == active) }">
         {{ item.name }}
         <br>
         {{ item.username }}
@@ -25,7 +25,8 @@ export default {
   data () {
     return {
       search: '',
-      results: []
+      results: [],
+      active: 0
     }
   },
   watch: {
@@ -43,6 +44,54 @@ export default {
   methods: {
     onInput () {
       this.getWord()
+    },
+    onUp ($event) {
+      if (this.hasInput()) {
+        $event.preventDefault()
+
+        this.active--
+        this.checkLimit()
+      }
+    },
+    onDown ($event) {
+      if (this.hasInput()) {
+        $event.preventDefault()
+
+        this.active++
+        this.checkLimit()
+      }
+    },
+    onEnter ($event) {
+      if (this.hasInput()) {
+        $event.preventDefault()
+
+        this.insertUser()
+        this.clearResults()
+      }
+    },
+
+    hasInput () {
+      return (this.search !== '' && this.results.length)
+    },
+    checkLimit () {
+      if (this.active < 0) {
+        this.active = 0
+      }
+      if (this.active >= this.results.length) {
+        this.active = this.results.length - 1
+      }
+    },
+
+    clearResults () {
+      this.search = ''
+      this.results = []
+      this.active = 0
+    },
+
+    insertUser () {
+      let item = this.results[this.active]
+
+      this.replaceWord(item.username)
     },
 
     getWord () {
@@ -62,7 +111,7 @@ export default {
 
         selection.removeAllRanges()
         selection.addRange(range)
-      } else if ((selection = document.selection) && selection.type !== 'Control') {
+      } else if ((selection = document.selection) && selection.createRange()) {
         range = selection.createRange()
         range.collapse(true)
         range.expand('word')
@@ -70,11 +119,71 @@ export default {
       }
 
       this.search = word
+    },
+    replaceWord (word) {
+      let selection
+      let range
+
+      word += '\u00A0'
+
+      if (window.getSelection && (selection = window.getSelection()).modify) {
+        selection.collapseToStart()
+        selection.modify('move', 'backward', 'word')
+        selection.modify('extend', 'forward', 'word')
+
+        range = selection.getRangeAt(0)
+
+        range.deleteContents()
+        range.insertNode(document.createTextNode(word))
+
+        selection.modify('move', 'forward', 'character')
+      } else if ((selection = document.selection) && selection.type !== 'Control') {
+        range = selection.createRange()
+        range.collapse(true)
+        range.expand('word')
+        range.text = word
+      }
     }
+
   }
 }
 </script>
 
 <style>
+.results_list {
+  background: #fff;
+  border: 1px solid #999;
+  border-radius: 2px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+}
 
+.results_item {
+  align-items: flex-start;
+  display: flex;
+  padding: 10px;
+}
+  .results_item.active {
+    background: #eee;
+  }
+
+.results_item_image {
+  border: 2px solid #fff;
+  border-radius: 100%;
+  display: block;
+  margin: 0 10px 0 0;
+  width: 30px;
+  height: 30px;
+}
+
+.results_item_name {
+  color: #000;
+  display: block;
+  font-size: 14px;
+}
+
+.results_item_username {
+  color: #666;
+  display: block;
+  font-size: 12px;
+}
 </style>
